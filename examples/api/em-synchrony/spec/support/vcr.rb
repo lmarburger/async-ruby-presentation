f9ef5b4561b54d6c19d852ccf5ef8fd300dcf717
Larry Marburger <larry@marburger.cc>
2011-08-18 16:20:21 -0400

    Remove api examples

--- examples/api/em-synchrony/spec/support/vcr.rb
+++
@@ -1,7 +0,0 @@
-require 'vcr'
-
-VCR.config do |c|
-  c.cassette_library_dir = 'spec/cassettes'
-  c.stub_with :webmock
-  c.default_cassette_options = { :record => :none }
-end
--- examples/api/goliath/spec/helper.rb
+++
@@ -1,3 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/spec'
-require 'wrong/adapters/minitest'
